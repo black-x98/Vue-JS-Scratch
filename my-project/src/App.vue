894990d5059e379ef0b0d0f73e7bdebe33cad183
Awlad <awlad@mouchak.com>
2020-09-08 12:40:32 +0600

    Ran my basic template in VueJS.

---
+++ my-project/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos"/>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos';
+
+export default {
+  name: "app",
+  components: {
+    Todos
+  },
+  data(){
+    return {
+      todos: [
+        {id: 1,
+          title: "Todo One",
+          completed: false},
+        {id: 2,
+          title: "Todo Two",
+          completed: true},
+        {id: 3,
+          title: "Todo Three",
+          completed: false}
+      ]
+    }
+  }
+
+}</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ my-project/src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>HEADER</h1>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <h3>{{todo.title}}</h3>
+    </div>
+    <footer>This is the footer.</footer>
+  </div>
+</template>
+
+<script>
+//import TodoItem from "@/components/TodoItem";
+
+export default {
+name: "Todos",
+  props: ["todos"]
+}
+
+</script>
+
+<style scoped>
+
+</style>
